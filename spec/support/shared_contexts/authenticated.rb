0530565fcc077adb607a533842c448135c2c278c
Ashwin <ashwin.santebennur@gmail.com>
2026-03-23 13:31:47 +0000

    fixed a typo issue

--- spec/support/shared_contexts/authenticated.rb
+++ spec/support/shared_contexts/authenticated.rb
@@ -1,7 +1,5 @@
-# spec/support/shared_contexts/authenticated.rb
 RSpec.shared_context "authenticated" do
   let(:password) { "password" }
-#   let(:user) { create(:user, password: password) }
   let(:user) { User.create!(name: 'tester', email_address: "test@example.com", password: password) }
   before { sign_in(user, password: password) }
 end
